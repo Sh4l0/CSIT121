d4ded328e2b9a492efadb3108a71693d6ba94bb0
Sh4l0 <142058797+Sh4l0@users.noreply.github.com>
2026-04-19 22:26:11 -0400

    Delete lab08 directory

--- lab08/script.js
+++
@@ -1,9 +0,0 @@
-/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("docMain").style.marginLeft = "250px";
-}
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("docMain").style.marginLeft = "0";
-}
